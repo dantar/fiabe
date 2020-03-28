3d6c320a07db2da1f78b523cda438d7d67d6dddf
Daniele <d.tarini@hyperborea.com>
2020-03-28 23:16:46 +0100

    Full screen toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fiabe';
+
+  d:any;
+  fullscreenEnabled: boolean;
+  isFullscreen: boolean;
+
+  ngOnInit(): void {
+    this.d = document;
+    this.isFullscreen = false;
+    if (this.d.fullscreenEnabled) {
+      this.fullscreenEnabled = true;
+    }
+  }
+
+  toggleFullScreen(event) {
+    this.isFullscreen = !this.isFullscreen;
+    if (this.isFullscreen) {
+      this.d.documentElement.requestFullscreen();
+    } else {
+      this.d.exitFullscreen();
+    }
+  }
+  
 }
