7d844f14ad22b20a90ae80cfc58ce5fe3a2e5267
Daniele <d.tarini@hyperborea.com>
2020-04-04 11:49:16 +0200

    favole.json moved to assets

--- src/app/services/audio-library.service.ts
+++ src/app/services/audio-library.service.ts
@@ -1,44 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Favola } from '../models/favola.model';
 import { GamesCommonService } from './games-common.service';
+import favole from '../../assets/favole.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AudioLibraryService {
 
-  favole: Favola[] = [
-    { id: 'favola-01',
-      titolo: 'L\'Apollonia della marmellata', 
-      audio: 'assets/favola-01.mpeg',
-      icon: 'assets/fiabe-01-sml.jpg',
-      image: 'assets/fiabe-01-sml.jpg',
-    },
-    { id: 'favola-02',
-      titolo: 'A toccare il naso del re', 
-      audio: 'assets/favola-02.mpeg',
-      icon: 'assets/fiabe-02-sml.jpg',
-      image: 'assets/fiabe-02-sml.jpg',
-    },
-    { id: 'favola-03',
-      titolo: 'Cucina spaziale', 
-      audio: 'assets/favola-03.mpeg',
-      icon: 'assets/fiabe-03-sml.jpg',
-      image: 'assets/fiabe-03-sml.jpg',
-    },
-    { id: 'favola-04',
-      titolo: 'Il palazzo di gelato', 
-      audio: 'assets/favola-04.mpeg',
-      icon: 'assets/fiabe-04-sml.jpg',
-      image: 'assets/fiabe-04-sml.jpg',
-    },
-    { id: 'favola-05',
-      titolo: 'Il topo che mangiava i gatti', 
-      audio: 'assets/favola-05.mpeg',
-      icon: 'assets/favola-05.jpg',
-      image: 'assets/favola-05.png',
-    },
-  ];
+  favole: Favola[] = favole;
 
   dial: Favola[];
   more: Favola[] = [];
