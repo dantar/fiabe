f42b649958b8eebbf0de85652efffb15a0c46512
Daniele <d.tarini@hyperborea.com>
2020-04-05 11:45:08 +0200

    Credits

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -16,7 +16,38 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
+  readers: string[]
+
   ngOnInit(): void {
+    this.readers = [
+      'Matteo Ambrosetti', 
+      'Davide Bartaletti', 
+      'Paolo Bonifati', 
+      'Francesco Borsacchi', 
+      'Ginevra Bruscino', 
+      'Carla Buscemi', 
+      'Eugenia Conti', 
+      'Giulia Del Nista', 
+      'Maria Falcone', 
+      'Filippo Fredducci', 
+      'Laura Lo Cascio', 
+      'Ginevra Mangano', 
+      'Cecilia Milocani', 
+      'Vincenzo Mirra', 
+      'Pierluigi Nieri', 
+      'Maria Pacelli', 
+      'Sara Pachetti', 
+      'Cosimo Parlangeli', 
+      'Eugenio Prina', 
+      'Rosa Maria Rizzi', 
+      'Cristiano Saggini', 
+      'Laura Salvetti', 
+      'Fabiola Santandrea', 
+      'Marco Saracco', 
+      'Daniele Tarini', 
+      'Francesca Torelli', 
+      'Paola Valenti',
+    ];
   }
 
 }
