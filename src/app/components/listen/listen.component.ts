d3782744a3211c0549b1572bc3dc69ffd9fba892
Daniele <d.tarini@hyperborea.com>
2020-04-05 10:14:04 +0200

    Listen dissolve

--- src/app/components/listen/listen.component.ts
+++ src/app/components/listen/listen.component.ts
@@ -2,11 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Favola } from 'src/app/models/favola.model';
 import { AudioLibraryService } from 'src/app/services/audio-library.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-listen',
   templateUrl: './listen.component.html',
-  styleUrls: ['./listen.component.scss']
+  styleUrls: ['./listen.component.scss'],
+  animations: [
+    trigger('dissolve', [
+      state('invisible', style({opacity: 0})),
+      state('visible', style({opacity: 1})),
+      transition(':enter', [style({opacity: 0}), animate('2s', style({opacity: 1}))]),
+      transition('invisible=>visible', [animate('100s')]),
+      transition('visible=>invisible', [animate('10s')]),
+    ]),
+  ]
 })
 export class ListenComponent implements OnInit {
 
