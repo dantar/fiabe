9765979d36b08061698ffa7ca7695b08c7b282f7
Daniele <d.tarini@hyperborea.com>
2020-04-05 10:57:51 +0200

    Browse all animation

--- src/app/components/browse-all/browse-all.component.ts
+++ src/app/components/browse-all/browse-all.component.ts
@@ -2,11 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { AudioLibraryService } from 'src/app/services/audio-library.service';
 import { Favola } from 'src/app/models/favola.model';
 import { Router } from '@angular/router';
+import { trigger, state, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-browse-all',
   templateUrl: './browse-all.component.html',
-  styleUrls: ['./browse-all.component.scss']
+  styleUrls: ['./browse-all.component.scss'],
+  animations: [
+    trigger('stream', [
+      transition(':enter', [
+        query('.browse-all-item', [
+          style({opacity: 0, transform: 'translateY(-300px)'}),
+          stagger('30ms', [
+            animate('300ms', style({opacity: 1, transform: 'none'}))
+          ])
+        ]),
+      ]),
+      transition(':leave', [style({opacity: 1}), animate('200ms', style({opacity: 0}))]),
+    ]),
+  ]
 })
 export class BrowseAllComponent implements OnInit {
 
